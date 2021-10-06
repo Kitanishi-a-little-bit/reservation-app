5a2f5216bd8967bb0b0aefe161436ffe32b857d2
Kitanishi-a-little-bit <a.little.bit.plus.hamamatsu@gmail.com>
2021-10-06 19:33:43 +0900

    ✨ Angularフロンドエンド側作成

---
+++ src/app/product/product-listings/product-listings.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../../product';
+
+@Component({
+  selector: 'app-product-listings',
+  templateUrl: './product-listings.component.html',
+  styleUrls: ['./product-listings.component.scss'],
+})
+export class ProductListComponent implements OnInit {
+  products: any;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.products = products;
+  }
+}
